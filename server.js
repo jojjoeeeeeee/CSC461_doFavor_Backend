fcffac0893f5aaec9cacf743c0c4ccaf607eb886
jojjoeeeeeee <jo-jo_kiki@hotmail.com>
2022-03-14 05:52:24 +0700

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const helmet = require('helmet');
+
+require('dotenv').config({ path: './src/.env' })
+
+app.use(helmet());
+const generator = require('generate-password');
+const corsOptions = {
+  exposedHeaders: 'Authorization',
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use('/public/img', express.static(__dirname + '/public/img'));
+app.use('/api', require('./src/middlewares/api'));
+
+const port = process.env.PORT || process.env.LOCAL_SERVER_PORT;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
---
+++ src/middlewares/api.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+require('../db');
+
+
+// router.use('/auth', require('../routes/auth'));
+
+module.exports = router;
