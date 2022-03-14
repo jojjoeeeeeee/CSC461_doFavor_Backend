ceaee7de65432129e3d3dfc4b6727b57a57c8647
jojjoeeeeeee <jo-jo_kiki@hotmail.com>
2022-03-15 06:23:00 +0700

    remove debug

--- src/db.js
+++ src/db.js
@@ -1,8 +1,4 @@
 const mongoose = require('mongoose');
-const fs = require('fs');
-const path = require('path');
-var publicKEY = fs.readFileSync(path.join(__dirname + '/public.key'), 'utf8');
-console.log(`publicKEY${publicKEY}`)
 
 mongoose
   .connect('mongodb+srv://jojjoeeeeeee:RgNWZfaPWBTslUea@cluster0.aun5d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
