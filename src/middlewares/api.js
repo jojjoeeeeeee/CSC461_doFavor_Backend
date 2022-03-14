84a7ead6c7c5a262e7ddd369338df1df5e5fa2e6
jojjoeeeeeee <jo-jo_kiki@hotmail.com>
2022-03-15 06:04:36 +0700

    test

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -6,6 +6,10 @@ require('../db');
 router.get('/test', (req,res) => {
     res.send("HELLOWORLD HEROKU")
 })
+
+router.get('/hi', (req,res) => {
+    res.send("HIIII")
+})
 // router.use('/auth', require('../routes/auth'));
 
 module.exports = router;
