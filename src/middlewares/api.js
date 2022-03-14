035e4b73869c93b333bdea6c8a59c8306b7fdee1
jojjoeeeeeee <jo-jo_kiki@hotmail.com>
2022-03-15 05:58:34 +0700

    deploy to heroku

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -3,6 +3,9 @@ const router = express.Router();
 require('../db');
 
 
+router.get('/', (req,res) => {
+    res.send("HELLOWORLD HEROKU")
+})
 // router.use('/auth', require('../routes/auth'));
 
 module.exports = router;
